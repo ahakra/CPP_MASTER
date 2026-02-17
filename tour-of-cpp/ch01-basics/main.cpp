8b85b1dd692bdc38025204360d388a29f8cdbe63
Ahmad Akra <aakra@vanrise.com>
2026-02-17 11:26:25 +0200

    tour of cpp

---
+++ tour-of-cpp/ch01-basics/main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by ahmad.akra on 2/17/2026.
+//
+
+#include "main.h"
+
+int main()
+{
+
+}
---
+++ tour-of-cpp/ch01-basics/main.h
@@ -0,0 +1,14 @@
+//
+// Created by ahmad.akra on 2/17/2026.
+//
+
+#ifndef CPP_MASTER_MAIN_H
+#define CPP_MASTER_MAIN_H
+
+
+class main {
+
+};
+
+
+#endif //CPP_MASTER_MAIN_H
